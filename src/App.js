b5ea9103261afda89c6b7c49dbc84b1f24d42747
salvainfantino <s.infantino@gmail.com>
2024-01-26 17:58:08 +1000

    pwa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // src/App.js
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Modifier ici
 
 import Header from './components/Header';
@@ -7,20 +7,38 @@ import Favorites from './components/Favorites';
 import PokemonList from './components/PokemonList';
 import PokemonDetail from './components/PokemonDetail'; // Assurez-vous d'importer le composant PokemonDetail
 import ErrorPage from './components/ErrorPage';
+import ErrorDevice from './components/ErrorDevice';
 
 import data from './doc/pokedex.json';
 
+
 const App = () => {
+
+  const [width, setWidth] = useState(window.innerWidth);
+  
+  function handleWindowSizeChange() {
+      setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+      window.addEventListener('resize', handleWindowSizeChange);
+      return () => {
+          window.removeEventListener('resize', handleWindowSizeChange);
+      }
+  }, []);
+
   return (
     <Router>
-      <Header />
-      <Routes> {/* Modifier ici */}
-        <Route path="/" element={<PokemonList data={data} />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/pokemon/:id" element={<PokemonDetail />} /> {/* Ajoutez cette ligne */}
-         <Route path="/error" element={<ErrorPage />} /> {/* Route par défaut pour les erreurs 404 */}
-         <Route path="/*" element={<ErrorPage />} /> {/* Route par défaut pour les erreurs 404 */}
-      </Routes> {/* Modifier ici */}
+      {width > 768 ? <ErrorDevice /> :
+      <>
+        <Header />
+        <Routes> {/* Modifier ici */}
+          <Route path="/" element={<PokemonList data={data} />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/pokemon/:id" element={<PokemonDetail />} /> {/* Ajoutez cette ligne */}
+          <Route path="/error" element={<ErrorPage />} /> {/* Route par défaut pour les erreurs 404 */}
+          <Route path="/*" element={<ErrorPage />} /> {/* Route par défaut pour les erreurs 404 */}
+        </Routes> {/* Modifier ici */}
+      </>}
     </Router>
   );
 }
---
+++ src/components/ErrorDevice.js
@@ -0,0 +1,10 @@
+const ErrorDevice = () => {
+    return (
+        <div className="d-flex justify-content-center align-items-center mt-5 text-center display-6">
+            Please, use a mobile device to enjoy this app
+            <br /> Thanks ;)
+        </div>
+    )
+}
+
+export default ErrorDevice;
